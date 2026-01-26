f91019594e5ac3971e49ee8f598574a506721a59
Saeedeh Karimi <saeedehkarimi1994@gmail.com>
2026-01-26 23:51:15 +0100

    added fetch methods(Get,Post,Delete...) for managing todoList

--- todoList/src/App.jsx
+++ todoList/src/App.jsx
@@ -6,44 +6,70 @@ import { TodoFilter } from './components/TodoFilter/TodoFilter.jsx';
 
 function App() {
 
-  const [todos, setTodos]= useState([]);
+  const [todos, setTodos] = useState([]);
   const [filters, setFilters] = useState({});
 
-  function fetchTodos(){
+  function fetchTodos() {
     fetch(`${import.meta.env.VITE_MOCKAPI_BASE_URL}todos`, {
       method: 'GET',
-      headers:{'content-type': 'application/json'},
+      headers: { 'content-type': 'application/json' },
     })
-     .then((response)=>{
-       if(response.ok){
-        return response.json();
-       }
-        })
-          .then((todos)=>{
-          setTodos(todos);
-     })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then((todos) => {
+        setTodos(todos);
+      })
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchTodos();
   }, [])
 
-  function handleCreate(newTodo){
-    setTodos((prevTodos)=> [...prevTodos, {...newTodo, id:`${prevTodos.length + 1}`}]);
-  };
-  function handleUpdate(id,updatedTodo){
-    setTodos((prevTodos)=> prevTodos.map((todo)=> todo.id === id ? updatedTodo : todo));
-  };
-  function handleDelete(id){
-    setTodos((prevTodos)=> prevTodos.filter((todo)=> todo.id !== id));
+  function handleCreate(newTodo) {
+    fetch(`${import.meta.env.VITE_MOCKAPI_BASE_URL}todos`, {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(newTodo)
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then(fetchTodos)
   };
 
-  function filterTodos(todo){
-    const {completed, priority}= filters;
-    
-    return(
-      (completed==='' || todo.completed === completed )&&
-      (priority==='' || todo.priority === priority)
+  function handleUpdate(id, updatedTodo) {
+     fetch(`${import.meta.env.VITE_MOCKAPI_BASE_URL}todos/${id}`, {
+      method: 'PUT',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(updatedTodo)
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then(fetchTodos)
+  };
+
+  function handleDelete(id) {
+     fetch(`${import.meta.env.VITE_MOCKAPI_BASE_URL}todos/${id}`, {
+      method: 'DELETE'
+    })
+      .then((response) => !!response.ok && response.json())
+      .then(fetchTodos)
+  };
+
+  function filterTodos(todo) {
+    const { completed, priority } = filters;
+
+    return (
+      (completed === '' || todo.completed === completed) &&
+      (priority === '' || todo.priority === priority)
     )
 
   }
@@ -51,15 +77,15 @@ function App() {
   return (
     <div className={styles.App}>
       <header className={styles.Header}>
-        <img className={styles.Logo} src='public/to-do.png'/>
+        <img className={styles.Logo} src='public/to-do.png' />
         <h2 className={styles.Title}>Todo List</h2>
       </header>
       <div className={styles.AppContainer}>
-        <TodoForm onCreate={handleCreate}/>
-        <TodoFilter onFilter={setFilters}/>
-        <TodoList todos={todos.filter(filterTodos)} onUpdate={handleUpdate} onDelete={handleDelete}/>
+        <TodoForm onCreate={handleCreate} />
+        <TodoFilter onFilter={setFilters} />
+        <TodoList todos={todos.filter(filterTodos)} onUpdate={handleUpdate} onDelete={handleDelete} />
       </div>
-      
+
     </div>
   )
 }
